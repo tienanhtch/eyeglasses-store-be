6f9ee174763f1aa8ee8c87babd0011650a3bbd20
Lo Tien Anh <tienanhtch@gmail.com>
2025-09-14 10:33:50 +0700

    Initial commit: Setup Spring Boot project structure for eyeglasses store API

---
+++ src/main/java/com/eyeglasses/eyeglasses_store/EyeglassesStoreApplication.java
@@ -0,0 +1,13 @@
+package com.eyeglasses.eyeglasses_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyeglassesStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyeglassesStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eyeglasses/eyeglasses_store/EyeglassesStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eyeglasses.eyeglasses_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EyeglassesStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
